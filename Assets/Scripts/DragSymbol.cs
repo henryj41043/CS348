9c91b913d462cf5b131e8bb728de37638108f9c0
Andrew Dougherty <ad646@drexel.edu>
2014-05-18 23:15:40 -0400

    Dragging symbols into a hard coded equation and evaluating the equation for correctness now supported

---
+++ Assets/Scripts/DragSymbol.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using NCalc;
+
+public class DragSymbol : UIDragDropItem {
+
+	Vector3 objectPos;
+
+	protected override void OnDragDropStart()
+	{
+		objectPos = gameObject.transform.position;
+
+		base.OnDragDropStart();
+	}
+
+	protected override void OnDragDropRelease (GameObject surface)
+	{
+		if(surface != null && surface.gameObject.name.Contains("missingPiece"))
+		{
+			GameController gc = GameObject.Find ("GameController").GetComponent<GameController>();
+
+			int startInx = surface.gameObject.name.LastIndexOf(")")+1;
+			
+			int inx = Convert.ToInt32(surface.gameObject.name.Substring(startInx));
+			
+			string value = gameObject.GetComponent<UILabel>().text;
+			
+			if (gc.eq[inx].Contains(value))
+			{
+				// Determine which of the symbols in the equation list are still valid
+				// and remove the invalid symbols
+				mCollider.enabled = false;
+				gc.updateEquation(inx, value);
+				gc.validateEquation();
+			}
+			else
+			{
+				gameObject.transform.position = objectPos;
+				surface = null;
+			}
+		}
+		else
+		{
+			gameObject.transform.position = objectPos;
+			surface = null;
+		}
+		base.OnDragDropRelease(surface);
+	}
+}
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using NCalc;
 
 public class GameController : MonoBehaviour
 {
 	bool gameOver = false;
+	bool equationSolved = false;
 
 	public GameObject EquationPrefab;
 	//public GameObject EqualSignPrefab;
@@ -13,25 +16,23 @@ public class GameController : MonoBehaviour
 
 	public List<List<string>> eq = new List<List<string>>();
 
-//	List<string> a0 = new List<string>(){"2"};
-//	List<string> a1 = new List<string>(){"+", "*"};
-//	List<string> a2 = new List<string>(){"4", "6"};
-//	List<string> a3 = new List<string>(){"8"};
-
-	void Start()
-	{
-		createEquation();
-	}
+	string _equation; 
+	string _saveEquation;
 
 	//Hard coded test equation. This will be replaced with some algorithm which generates equations
 	List<List<string>> A = new List<List<string>>()
 	{
 		new List<string>(){"2"}, //first operand
-		new List<string>(){"+", "*"}, //potential operators
+		new List<string>(){"+", "x"}, //potential operators
 		new List<string>(){"4", "6"}, //potential second operand
 		new List<string>(){"="},
 		new List<string>(){"8"} //answer
 	};
+
+	void Start()
+	{
+		createEquation();
+	}
 
 	// Update is called once per frame
 	void Update ()
@@ -39,6 +40,42 @@ public class GameController : MonoBehaviour
 		if(gameOver)
 		{
 			Application.LoadLevel("GameOverScene");
+		}
+		else if(equationSolved)
+		{
+			clearEquation();
+		}
+	}
+
+	public void updateEquation(int inx, string value)
+	{
+		int i = _equation.IndexOf("{T" + inx + "}");
+
+		if (i > -1)
+		{
+			_equation = _equation.Remove(i, 4).Insert(i, value);
+		}
+	}
+
+	void clearEquation()
+	{
+		foreach(GameObject missingPiece in GameObject.FindGameObjectsWithTag("Missing Piece"))
+		{
+			NGUITools.Destroy(missingPiece.transform.GetChild(0).transform.GetChild(0).gameObject);
+		}
+		_equation = _saveEquation;
+		equationSolved = false;
+
+		eq.Clear();
+		for(int i = 0; i < A.Count; i++)
+		{
+			List<string> eqPart = new List<string>();
+			for(int j = 0; j < A[i].Count; j++)
+			{
+				eqPart.Add(A[i][j]);
+			}
+			
+			eq.Add(eqPart);
 		}
 	}
 
@@ -82,25 +119,82 @@ public class GameController : MonoBehaviour
 				symbolValue.text = eq[i][0];
 
 				elementWidth = symbolValue.width;
+
+				_equation += eq[i][0];
 			}
 			else
 			{
 				GameObject emptyBoxPrefab = (GameObject)Instantiate(MissingPiecePrefab, equationPrefab.transform.position, Quaternion.identity);
 				emptyBoxPrefab.name = emptyBoxPrefab.name + i;
+				emptyBoxPrefab.tag = "Missing Piece";
 				emptyBoxPrefab.transform.parent = equationPrefab.transform;
 
 				UISprite boxSprite = GameObject.Find(emptyBoxPrefab.name).GetComponent<UISprite>();
 				emptyBoxPrefab.transform.localPosition = new Vector3(startPosX + 20f, 0, 0);
+				emptyBoxPrefab.transform.localScale = Vector3.one;
 
 				elementWidth = boxSprite.width - 20f;
+				_equation += "{T" + i + "}"; 
 			}
 
 			startPosX += elementWidth + offset;
 		}
 
+		_saveEquation = _equation;
 		//GameObject equalPrefab = (GameObject)Instantiate(EqualSignPrefab, equationPrefab.transform.position, Quaternion.identity);
 		//equalPrefab.transform.parent = equationPrefab.transform;
 		//equalPrefab.transform.localPosition = new Vector3(startPosX - (offset * i), 0, 0)
+	}
+
+	public void validateEquation()
+	{
+
+		//int count = _equation.Split("{T").Length - 1;
+
+		List<string> tokens = new List<string>();
+
+		string tempEQ = _equation;
+		int pos = tempEQ.IndexOf("{T");
+		while (pos > -1 && pos < tempEQ.Length)
+		{
+			tokens.Add(tempEQ.Substring(pos,4));
+			pos = tempEQ.IndexOf("{T", pos + 1);
+		}
+
+		List<string> tempList = new List<string>();
+		for (int i = 0; i < tokens.Count; i++)
+		{
+			int inx = Convert.ToInt16(tokens[i].Substring(2,1));
+			for (int j = 0; j < eq[inx].Count; j++)
+			{
+				tempEQ = _equation;
+				int k = tempEQ.IndexOf(tokens[i]);
+				tempEQ = tempEQ.Remove(k, 4).Insert(k, eq[inx][j]);
+				if (!verifySymbols(tempEQ))
+					//eq[inx].Remove(eq[inx][j]);
+					tempList.Add(eq[inx][j]);
+			}
+
+			foreach(string s in tempList)
+			{
+				eq[inx].Remove(s);
+			}
+		}
+
+		if (_equation.IndexOf("{T") == -1)
+		{
+			//clearEquation();
+			equationSolved = true;
+		}
+	}
+
+	bool verifySymbols(string eq)
+	{
+		eq = eq.Replace("x", "*");
+
+		Expression expr = new Expression(eq);
+
+		return (bool)expr.Evaluate();
 	}
 
 }
